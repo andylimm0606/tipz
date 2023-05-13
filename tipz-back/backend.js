84c6a3e228fa39b011602f5dafdfb13c549f4e45
RedOrBlu <73367549+wbrandon25@users.noreply.github.com>
2023-05-13 11:55:13 -0700

    add random tip

--- tipz-back/backend.js
+++ tipz-back/backend.js
@@ -1,10 +1,13 @@
 const express = require("express")
 const app = express()
+const axios = require('axios')
 const mongoose = require("mongoose")
 const Message = require("./models/Message")
 const port = 8000
 
 app.use(express.json())
+
+require('dotenv').config();
 
 const mongoURI = "mongodb+srv://user2:aZOW45E2IqH0ZqsB@cluster0.tyiyisl.mongodb.net/"
 
@@ -41,7 +44,7 @@ app.get("/add_message", async (req, res) => {
 
 app.get("/get_all_messages", async (req, res) => {
     try {
-		
+
 		const message = await Message.find().sort({ _id: -1 }) // sort by most recently added
         res.status(200).json(message)
 
@@ -53,6 +56,36 @@ app.get("/get_all_messages", async (req, res) => {
     }
 })
 
+app.get("/random_tip", async (req, res) => {
+    const message = "Write a single tip for sustainability, your response should be one complex sentence. Do not append any other commentary to the beginning or the end"
+
+    try {
+        const response = await axios.post(
+            "https://api.openai.com/v1/chat/completions",
+            {
+                model: "gpt-3.5-turbo",
+                messages: [
+                    { role: "system", content: "You are a helpful assistant." },
+                    { role: "user", content: message },
+                ],
+            },
+            {
+				headers: {
+					'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+					'Content-Type': 'application/json',
+				  },
+            }
+        )
+
+        const reply = response.data.choices[0].message.content
+
+        res.json({ reply })
+    } catch (error) {
+        console.error("Error:", error)
+        res.status(500).json({ error: "Something went wrong" })
+    }
+})
+  
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
