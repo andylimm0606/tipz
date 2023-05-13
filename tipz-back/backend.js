d76aa672d3e29d4e751c38b985bcdb78cf7646d3
RedOrBlu <73367549+wbrandon25@users.noreply.github.com>
2023-05-13 10:48:24 -0700

    add mongoDB conneciton

--- tipz-back/backend.js
+++ tipz-back/backend.js
@@ -1,14 +1,43 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const User = require('./models/Message'); 
 const port = 8000;
 
 app.use(express.json());
 
+const mongoURI = 'mongodb+srv://user2:aZOW45E2IqH0ZqsB@cluster0.tyiyisl.mongodb.net/';
+
+// Connect to MongoDB
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
+});
+app.get('/test_write', async (req, res) => {
+    try {
+		// const { username, message } = req.body; // edit this later to grab info from request body
+		const username = '2joe'
+		const message = 'This is a test message with new'
+		
+		const user = new User({ username, message });
+	
+		await user.save();
+	
+		res.status(201).json({ message: 'Data saved successfully' });
+	  } catch (error) {
+		console.error('Error saving data:', error);
+		res.status(500).json({ message: 'An error occurred while saving data' });
+	  }
 });
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
-
